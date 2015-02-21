d055073d50bb12f6df34e5e8aa50a348e8d8fde4
= <=>
2015-02-21 19:45:20 -0200

    primeira funcao implementadas

---
+++ ep1.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+;EP1 de Logica
+;Algoritmo para encontrar um feixo transitivo reflexivo
+
+;definicao de tupla
+(define (tupla x y)
+  (list x y))
+
+;conjunto de tuplas definido na mao
+(define (conjunto a b c d) (
+                      list a b c d))
+
+;conjunto efetivo que iremos percorrer
+(define arg (conjunto (tupla 1 2) (tupla 3 4) (tupla 4 5) (tupla 6 7)))
+
+
+;item 1 - funcao que percorre lista
+
+;funcao auxiliar for
+(define (percorreConjunto myconj)
+  (if (empty? myconj)
+      ;fim da recursao
+      (display "")
+      ;esse begin e necessario para executarmos multiplos procedures dentro do if
+      (begin 
+        ;display dos dados
+        (display (first myconj))
+        ;recursao que permite percorrer atraves dos conjuntos...
+        (percorreConjunto (rest myconj))
+      )
+   )
+)
+
+;chamando a funcao definida anteriormente - teste
+(percorreConjunto arg)
+
+;percorrendo uma relacao binaria
+
+
+
+  
+
