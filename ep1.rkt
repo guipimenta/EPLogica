a7b1063d0506ac2aa1e6cee37f59ccc0b7f9e1d2
tomasaazevedo <tomasaazevedo@gmail.com>
2015-03-13 11:40:58 -0300

    ep1 final

--- ep1.rkt
+++ ep1.rkt
@@ -23,9 +23,6 @@
    )
 )
 
-;chamando a funcao definida anteriormente - teste
-;(percorreConjunto dpair_list)
-
 ;
 ;     PARTE 2
 ;
@@ -45,8 +42,6 @@
   )
 )
 
-;(percorreBinaria dpair_list 4)
-
 ;
 ;     PARTE 3
 ;
@@ -54,18 +49,6 @@
 ;fecho reflexivo
 
 ;obs.: foi considerado que o fecho reflexivo leva em conta ambos o domínio e o contra-domínio
-
-;funcao que adiciona um par a uma lista caso esse par ainda não esteja na lista
-(define (adicionaParEmLista par rbin)
-  (if (empty? rbin)
-    (list par)
-    (if (member par rbin)
-      rbin
-      (append rbin (list par))
-    )
-  )
-)
-
 
 ;funcao que calcula o fecho reflexivo dos PRIMEIROS elementos de uma lista de pares ordenados
 (define (determinaFechoReflexivo1 rbin freflexivo original)
@@ -91,14 +74,14 @@
 
 ;funcao que junta os dois fechos reflexivos parciais
 (define (determinaFechoReflexivo rbin)
-  (concatListas (determinaFechoReflexivo1 dpair_list '() dpair_list) (determinaFechoReflexivo2 dpair_list '() dpair_list))
+  (concatListas (determinaFechoReflexivo1 rbin '() rbin) (determinaFechoReflexivo2 rbin '() rbin))
 )
 
 
 ;fecho transitivo
 
 (define (determinaFechoTransitivo rbin)
-  (determinaFechoTransitivoAux rbin '())
+  (removerElementosDeL1EmL2 (concatListas (determinaFechoTransitivoAux rbin '()) (determinaFechoTransitivoAux (reverse rbin) '())) rbin)
 )
 
 (define (determinaFechoTransitivoAux rbin ftransitivo)
@@ -115,18 +98,37 @@
 ;e retorna uma lista com os pares ordenados que completam o fecho transitivo (ex.: ((1,3) (1,4)))
 (define (criaParesParaFechoTransitivo par1 listaDePares2 listaNova)
   (if (empty? listaDePares2)
-      listaNova
-      (criaParesParaFechoTransitivo par1 (cdr listaDePares2) (adicionaParEmLista (cons (car par1) (cdar listaDePares2)) listaNova))
-
-   )
+    listaNova
+    (begin
+      ;apenas adiciona par ordenado transitivo se nao for da forma (x,x)
+      (if (equal? (car par1) (cdar listaDePares2))
+        (criaParesParaFechoTransitivo par1 (cdr listaDePares2) listaNova)
+        (criaParesParaFechoTransitivo par1 (cdr listaDePares2) (adicionaParEmLista (cons (car par1) (cdar listaDePares2)) listaNova))
+      )
+    )
+  )
 )
 
 ;funcao que obtem um fecho reflexivo e transitivo chamando as funcoes de cada fecho
-(define (determinaFechoReflexivoTransitivo dpair_list)
-  (concatListas (determinaFechoReflexivo dpair_list) (determinaFechoTransitivo dpair_list))
+(define (determinaFechoReflexivoTransitivo rbin)
+  (concatListas (determinaFechoReflexivo rbin) (determinaFechoTransitivo rbin))
 )
 
-;funcao auxiliar para concatenar duas listas de pares ordenados retirando repeticoes
+
+;funcoes auxiliares
+
+;funcao que adiciona um par a uma lista caso esse par ainda não esteja na lista
+(define (adicionaParEmLista par rbin)
+  (if (empty? rbin)
+    (list par)
+    (if (member par rbin)
+      rbin
+      (append rbin (list par))
+    )
+  )
+)
+
+;funcao para concatenar duas listas de pares ordenados retirando repeticoes
 (define (concatListas lista1 lista2)
   (removerDuplicatas (append lista1 lista2))
 )
@@ -143,22 +145,109 @@
   )
 )
 
-;exemplo de como utilizar
+(define (removerElementosDeL1EmL2 lista1 lista2)
+  (cond ((null? lista1)
+         '())
+        ((member (car lista1) lista2)
+         (removerElementosDeL1EmL2 (cdr lista1) lista2))
+        (else
+         (cons (car lista1) (removerElementosDeL1EmL2 (cdr lista1) lista2))
+        )
+  )
+)
 
-;conjunto de pares ordenados a ser utilizado como exemplo
-(define dpair_list (list (cons 'ab 'ce) (cons 3 4) (cons 4 5) (cons 4 6) (cons 'ce 'cf) (cons 'ce 8)))
+;
+;Casos de teste
+;
 
-(display "Relacao = ")
-(display dpair_list)
-(display "\n\n")
+;caso de teste 1
+;B = ((1,2) (2,3) (2,4))
+;R = ((1,1) (2,2) (3,3) (4,4))
+;T = ((1,3) (1,4))
+;RT = ((1,1) (2,2) (3,3) (4,4) (1,3) (1,4))
 
-(display "Fecho Reflexivo = ")
-(display (determinaFechoReflexivo dpair_list))
-(display "\n")
-(display "Fecho Transitivo = ")
-(display (determinaFechoTransitivo dpair_list))
-(display "\n")
-(display "Fecho Reflexivo e Transitivo = ")
-(display (determinaFechoReflexivoTransitivo dpair_list))
-(display "\n")
+;(define relacaoB (list (cons 1 2) (cons 2 3) (cons 2 4) (cons 3 1)))
+
+;(display "Relacao = ")
+;(display relacaoB)
+;(display "\n\n")
+
+;(display "Fecho Reflexivo = ")
+;(display (determinaFechoReflexivo relacaoB))
+;(display "\n")
+;(display "Fecho Transitivo = ")
+;(display (determinaFechoTransitivo relacaoB))
+;(display "\n")
+;(display "Fecho Reflexivo e Transitivo = ")
+;(display (determinaFechoReflexivoTransitivo relacaoB))
+;(display "\n")
+
+
+;caso de teste 2
+;B = ((1,1) (1,2) (1,4) (2,3) (2,4) (3,2) (4,1))
+;R = ((2,2) (3,3) (4,4))
+;T = ((1,3) (2,1) (3,4) (4,2))
+;RT = ((2,2) (3,3) (4,4) (1,3) (2,1) (3,4) (4,2))
+
+;(define relacaoB (list (cons 1 1) (cons 1 2) (cons 1 4) (cons 2 3) (cons 2 4) (cons 3 2) (cons 4 1)))
+
+;(display "Relacao = ")
+;(display relacaoB)
+;(display "\n\n")
+
+;(display "Fecho Reflexivo = ")
+;(display (determinaFechoReflexivo relacaoB))
+;(display "\n")
+;(display "Fecho Transitivo = ")
+;(display (determinaFechoTransitivo relacaoB))
+;(display "\n")
+;(display "Fecho Reflexivo e Transitivo = ")
+;(display (determinaFechoReflexivoTransitivo relacaoB))
+;(display "\n")
+
+
+;caso de teste 3
+;B = ((ab,ce) (3,4) (2a,3P7) (cf,1) (ce,ab) (4,ab) (7f3v,bcD81) (7f3v,7f3v) (45b,2a) (4,1) (1,ab))
+;R = ((ab,ab) (3,3) (2a,2a) (cf, cf) (ce,ce) (4,4) (45b,45b) (1,1) (3P7,3P7) (bcD81))
+;T = ((3,ab) (3,1) (cf,ab) (1,ce) (45b,3P7) (4,ce))
+;RT = ((ab,ab) (3,3) (2a,2a) (cf, cf) (ce,ce) (4,4) (45b,45b) (1,1) (3P7,3P7) (bcD81) (3,ab) (3,1) (cf,ab) (1,ce) (45b,3P7) (4,ce))
+
+;(define rbin (list (cons 'ab 'ce) (cons 3 4) (cons '2a '3P7) (cons 'cf '1) (cons 'ce 'ab) (cons '4 'ab) (cons '7f3v 'bcD81) (cons '7f3v '7f3v) (cons '45b '2a) (cons 4 1) (cons '1 'ab)))
+
+;(display "Relacao = ")
+;(display rbin)
+;(display "\n\n")
+
+;(display "Fecho Reflexivo = ")
+;(display (determinaFechoReflexivo rbin))
+;(display "\n")
+;(display "Fecho Transitivo = ")
+;(display (determinaFechoTransitivo rbin))
+;(display "\n")
+;(display "Fecho Reflexivo e Transitivo = ")
+;(display (determinaFechoReflexivoTransitivo rbin))
+;(display "\n")
+
+
+;caso de teste 4
+;B = ()
+;R = ()
+;T = ()
+;RT = ()
+
+;(define rbin (list))
+
+;(display "Relacao = ")
+;(display rbin)
+;(display "\n\n")
+
+;(display "Fecho Reflexivo = ")
+;(display (determinaFechoReflexivo rbin))
+;(display "\n")
+;(display "Fecho Transitivo = ")
+;(display (determinaFechoTransitivo rbin))
+;(display "\n")
+;(display "Fecho Reflexivo e Transitivo = ")
+;(display (determinaFechoReflexivoTransitivo rbin))
+;(display "\n")
 
