0f277055ffa8c4445edf9b8414a5b0923c7c6d28
tomasaazevedo <tomasaazevedo@gmail.com>
2015-04-05 11:08:15 -0300

    modificacao gigante: obtem as frases para multiplas regras

--- ep2.rkt
+++ ep2.rkt
@@ -1,7 +1,7 @@
 #lang racket
 ; Exercicio-Programa 2: recursividade para reconhecimento de cadeias
 ; Grupo: Guilherme Pimenta Sorregotti
-;        Tomas Azevedo
+;        Tomás Albuquerque Azevedo
 
 
 ;
@@ -12,9 +12,133 @@
 ;            ex: A->abc B->aBa
 ;                Regras: ((("A") ("a" "b" "c")), ( "B" ("aBa")))
 ;                NT: ("A" "B")
-(define G (list (list "S" (list "A" "a")) (list "A" (list "b" "B")) (list "B" (list "b")) ) )
-(define NT (list "S" "A" "B"))
+(define G (list (list "S" (list "a" "b" "A")) (list "A" (list "a" "A")) (list "A" (list "c" "A")) ) )
+(define NT (list "S" "A"))
 
+
+; Descricao:
+;            Funcao auxiliar para determinar se um elemento faz parte de uma lista
+; Entrada:
+;   elemento: ELEMENTO a ser procurado na lista
+;      lista:  lista aonde o elemento anterior vai ser procurado
+; saida:
+;       true: pertence
+;      false: nao pertence
+(define (isMember element list)
+  (if (empty? list)
+      #f
+      (if (list? (member element list) )
+          #t
+          (isMember element (rest list))
+      )
+  )
+)
+
+; Descricao:
+;            Funcao que retorna parte de uma lista
+; Entrada:
+;      lst: lista a ser manipulada
+;      start: posicao do primeiro caracter da novalista (comeca em 1)
+;      count: contagem de caracteres a serem extraidos
+; saida:
+;       lista nova
+(define (slice lst start count)
+  (if (> start 1)
+      (slice (cdr lst) (- start 1) count)
+      (get-n-items lst count)
+  )
+)
+
+; Descricao:
+;            Funcao a ser utilizada pela funcao SLICE, que retorna N itens da uma lista
+; Entrada:
+;      lst: lista a ser manipulada
+;      num: numero de itens a serem extraidos
+; saida:
+;       lista nova
+(define (get-n-items lst num)
+  (if (> num 0)
+      (cons (car lst) (get-n-items (cdr lst) (- num 1)))
+      '()
+  )
+)
+
+; Descricao:
+;            Funcao que determina se um elemento é membro de uma lista
+; Explicacao:
+;            A Funcao utiliza as funcoes isMemberAux1 e isMemberAux2 para separar os casos de
+;      uma lista simples e uma lista de listas, respectivamente
+; Entrada:
+;      element: elemento a ser procurado na lista
+;      lista: lista a ser manipulada
+; saida:
+;       true: se elemento pertence a lista
+;       false: se elemento nao pertence a lista
+(define (isMember2 element lista)
+  (if (empty? lista)
+      #f
+      (if (list? (first lista))
+          (isMemberAux1 element lista #f)
+          (isMemberAux2 element lista #f)
+      )
+  )
+)
+
+; Descricao:
+;            Funcao que determina se um elemento é membro de uma lista de listas
+; Entrada:
+;      element: elemento a ser procurado na lista
+;      lista: lista a ser manipulada
+;      return: variavel auxiliar de retorno da funcao
+; saida:
+;       true: se elemento pertence a lista
+;       false: se elemento nao pertence a lista
+(define (isMemberAux1 element lista return)
+  (if (empty? element)
+      #t
+      (if (list? element)
+          (if (list? (first element))
+              (if (< (length lista) (length element))
+                  return
+                  (isMemberAux1 element (rest lista) (or (equal? element (slice lista 1 (length element))) return))
+              )
+              (if (< (length lista) 1)
+                  return
+                  (isMemberAux1 element (rest lista) (or (equal? (list element) (slice lista 1 1)) return))
+              )
+          )
+          (if (< (length lista) 1)
+              return
+              (isMemberAux1 element (rest lista) (or (equal? (list (list element)) (slice lista 1 1)) return))
+          )
+      )
+  )
+)
+
+; Descricao:
+;            Funcao que determina se um elemento é membro de uma lista simples
+; Entrada:
+;      element: elemento a ser procurado na lista
+;      lista: lista a ser manipulada
+;      return: variavel auxiliar de retorno da funcao
+; saida:
+;       true: se elemento pertence a lista
+;       false: se elemento nao pertence a lista
+(define (isMemberAux2 element lista return)
+  (if (empty? element)
+      #t
+      (if (list? element)
+          (if (< (length lista) (length element))
+              return
+              (isMemberAux2 element (rest lista) (or (equal? element (slice lista 1 (length element))) return))
+          )
+          (if (< (length lista) 1)
+              return
+              (isMemberAux2 element (rest lista) (or (equal? (list element) (slice lista 1 1)) return))
+          )
+      )
+  )
+)
 
 ; Descricao:
 ;              Funcao auxiliar que identifica TODOS os nao-terminais em uma frase
@@ -28,7 +152,7 @@
 (define (procuraNT frase NT NTList)
   (if (empty? NT)
       NTList ;frase so tem nao terminais
-      (if (isMember (first NT) frase)
+      (if (isMember2 (first NT) frase)
           (procuraNT frase (rest NT) (append NTList (list (first NT))))
           (procuraNT frase (rest NT) NTList)
       )
@@ -36,23 +160,37 @@
 )
 
 ; Descricao:
-;            Funcao auxiliar para determinar se um elemento faz parte de uma lista
+;              Funcao auxiliar que identifica TODOS os nao-terminais em múltiplas frases
 ; Entrada:
-;   elemento: ELEMENTO a ser procurado na lista
-;      lista:  lista aonde o elemento anterior vai ser procurado
+;      frases:  LISTA de listas aonde a funcao vai verificar a existencia de NT
+;         NT:  LISTA de elementos nao terminais a serem procurados
+;     NTList:  LISTA vazia
 ; saida:
-;       true: pertence
-;      false: nao pertence
-(define (isMember element list)
-  (if (empty? list)
-      #f
-      (if (list? (member element list  ))
-          #t
-          (isMember element (rest list))
+;     NTLIST:  LISTA com todos NT terminais pertencentes a frase
+(define (procuraNT2 frases NT NTList)
+  (if (empty? frases)
+      NTList ;frase so tem nao terminais
+      (if (list? (first frases))
+          (procuraNT2 (rest frases) NT (appendToListIfNotPresent (procuraNT (first frases) NT (list)) NTList))
+          (procuraNT2 (list) NT (appendToListIfNotPresent (procuraNT frases NT (list)) NTList))
       )
-  )
+   )
 )
 
+; Descricao:
+;              Funcao auxiliar que identifica TODAS as regras de um conjunto de nao-terminais
+; Entrada:
+;      NTList:  LISTA de listas aonde a funcao vai verificar a existencia de NT
+;          G:  LISTA de regras (gramática) disponíveis
+;     regras:  variavel auxiliar de retorno (inicialmente nula)
+; saida:
+;     regras  LISTA com todas as regras de uma lista de terminais
+(define (procuraRegrasConjuntoNT NTList G regras)
+  (if (empty? NTList)
+      regras
+      (procuraRegrasConjuntoNT (rest NTList) G (procuraRegra (first NTList) G regras))
+  )
+)
 
 ;
 ; Descricao:
@@ -67,7 +205,7 @@
 (define (procuraRegra NT G regras)
   (if (empty? G)
       regras
-      (if (isMember NT (list (first (first G))))
+      (if (isMember2 NT (list (first (first G))))
           (procuraRegra NT (rest G) (append (list (first G) ) regras  ))
           (procuraRegra NT (rest G) regras)
        )
@@ -89,7 +227,7 @@
       (reverse novafrase)
       (if (empty? regra)
           (substituiNTFrase (rest frase) regra (append (list (first frase)) novafrase))
-          (if (isMember (first (first regra)) (list (first frase)))
+          (if (isMember2 (first (first regra)) (list (first frase)))
               (substituiNTFrase (rest frase) (list) (append (reverse (second (first regra))) novafrase))
               (substituiNTFrase (rest frase) regra (append (list (first frase)) novafrase))
               )
@@ -97,23 +235,103 @@
   )
 )
 
-(define (procuraRegrasConjuntoNT NTList G regras)
-  (if (empty? NTList)
-      regras
-      (procuraRegrasConjuntoNT (rest NTList) G (procuraRegra (first NTList) G regras))
+; Descricao:
+;              Substitui todas as regras nos terminais da frase
+; Argumentos de entrada:
+;       frase: lista que denota uma frase
+;       regras: múltiplas regras na codificao padrao adotada pelo EP (vide comentarios acima)
+;   novafrase: nova frase, substituindo a primeira ocorrencia do NT pela regra
+; Saida:
+;   novafrase: nova frase, com um NT substituido pela regra
+(define (substituiNTFrase2 frase regras novafrase)
+  (if (empty? regras)
+      novafrase
+      (if (empty? novafrase)
+          (substituiNTFrase2 frase (rest regras) (append (substituiNTFrase frase (list (first regras)) (list)) novafrase))
+          (if (list? (first novafrase))
+              (substituiNTFrase2 frase (rest regras) (append (list (substituiNTFrase frase (list (first regras)) (list))) novafrase))
+              (substituiNTFrase2 frase (rest regras) (append (list (substituiNTFrase frase (list (first regras)) (list))) (list novafrase)))
+          )
+      )
+  )
+)
+
+; Descricao:
+;              Substitui todas as regras em todas as frases passadas
+; Argumentos de entrada:
+;       frases: lista de listas que denota multiplas frases
+;       regras: múltiplas regras na codificao padrao adotada pelo EP (vide comentarios acima)
+;   novasfrases: novas frases, substituindo a primeira ocorrencia do NT pela regra
+; Saida:
+;   novasfrases: novas frases, com um NT substituido pela regra
+(define (substituiNTFrase3 frases regras novasfrases)
+  (if (empty? frases)
+      novasfrases
+      (if (list? (first frases))
+          (substituiNTFrase3 (rest frases) regras (appendToListIfNotPresent (substituiNTFrase2 (first frases) regras (list)) novasfrases))
+          (substituiNTFrase3 (list) regras (appendToListIfNotPresent (substituiNTFrase2 frases regras (list)) novasfrases))
+      )
   )
 )
 
 ;teriamos que ter um protipo desse tipo
 ;que lindo!
 (define (achaTodasFraseProfundidadeN SI G NT novafrase SIZE )
-  (display SI)
-  (newline)
-  (if (or (>= (length SI) SIZE) (= (length (procuraNT SI NT (list))) 0)  )
-      SI
-      (achaTodasFraseProfundidadeN (substituiNTFrase SI (procuraRegrasConjuntoNT (procuraNT SI NT (list)) G (list)) (list)) G NT (list) SIZE )
+  (if (empty? SI)
+      novafrase
+      (achaTodasFraseProfundidadeN (frasesDeTamanhoNOuMenor (substituiNTFrase3 SI (procuraRegrasConjuntoNT (procuraNT2 SI NT (list)) G (list)) (list)) SIZE (list)) G NT (appendToListIfNotPresent SI novafrase) SIZE )
   )
 )
+
+;funcao que retorna as frases (de uma lista de frases) com tamanho menor ou igual a SIZE
+(define (frasesDeTamanhoNOuMenor frases SIZE novasfrases)
+  (if (empty? frases)
+      novasfrases
+      (if (list? (first frases))
+          (if (<= (length (first frases)) SIZE)
+              (frasesDeTamanhoNOuMenor (rest frases) SIZE (appendToListIfNotPresent (first frases) novasfrases))
+              (frasesDeTamanhoNOuMenor (rest frases) SIZE novasfrases)
+          )
+          (if (<= (length frases) SIZE)
+              (frasesDeTamanhoNOuMenor (list) SIZE (appendToListIfNotPresent frases novasfrases))
+              (frasesDeTamanhoNOuMenor (list) SIZE novasfrases)
+          )
+      )
+  )
+)
+
+;funcao que dá um append de um elemento no final de uma lista
+(define (appendToList element lista)
+  (if (empty? lista)
+      (if (list? element)
+          element
+          (list element)
+      )
+      (if (list? (first lista))
+          (append lista (list element))
+          (append (list lista) (list element))
+      )
+  )
+)
+
+;funcao que  dá um append de um elemento no final de uma lista caso esse elemento ainda nao pertenca a lista
+(define (appendToListIfNotPresent element lista)
+  (if (empty? element)
+      lista
+      (if (list? (first element))
+          (if (isMember2 (first element) lista)
+              (appendToListIfNotPresent (rest element) lista)
+              (appendToListIfNotPresent (rest element) (appendToList (first element) lista))
+          )
+          (if (isMember2 element lista)
+              (appendToListIfNotPresent (list) lista)
+              (appendToListIfNotPresent (list) (appendToList element lista))
+          )
+      )
+  )
+)
+
+
 (define listaRegras (list))
 ;que eu quero chegar
 (display "Gramatica: ")
@@ -133,10 +351,10 @@
 ;(define novafrase2(substituiNTFrase novafrase regras2 (list)))
 ;(display novafrase2)
 
-(achaTodasFraseProfundidadeN (list "S") G NT (list) 10)
-
-
-
-
+;(define regras (list (list "A" (list "a" "c"))))
+;(define regras (list (list "A" (list "a" "c")) (list "A" (list "a" "b")) ) )
+;(define frases (list (list "a" "b" "A") (list "d" "d" "A")) )
+;(substituiNTFrase3 frases regras (list))
+(achaTodasFraseProfundidadeN (list "S") G NT (list) 6)
 
 ;(substituiNTRegra frase (procuraRegra (procuraNT (list "A" "b" "c") NT) G listaRegras) (list))
